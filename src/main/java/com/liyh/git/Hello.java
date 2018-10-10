26c85e2159d01e6f8550c74b4b3e30b6d5f004cd
liyh <liyh@AsiaInfo.com>
2018-10-10 16:42:30 +0800

    编辑信息

--- src/main/java/com/liyh/git/Hello.java
+++ src/main/java/com/liyh/git/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Testgit......");
 		System.out.println("eclipse.........");
+		System.out.println("eclipse hot_fit .........");
 	}
 
 }
